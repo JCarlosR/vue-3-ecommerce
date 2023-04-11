2b0a7ea17aa1f544806ddc501ce47a7b5880efd8
Juan Ramos <juancagb.17@gmail.com>
2023-04-10 23:31:57 -0500

    Move json files to src/data if we want to import

--- src/stores/categories.ts
+++ src/stores/categories.ts
@@ -1,17 +1,10 @@
 import { defineStore } from 'pinia'
 import type { Category } from '@/model/types';
+import categoriesData from '../data/categories.json'
 
 export const useCategoriesStore = defineStore('categories', {
     state: () => ({
-        categories: [{
-            id: 1,
-            name: 'Oficina',
-            description: 'Productos para tu oficina.'
-        }, {
-            id: 2,
-            name: 'Computadora',
-            description: 'Accesorios para computadora.'
-        }] as Category[]
+        categories: categoriesData as Category[]
     }),
     getters: {
         
--- src/stores/products.ts
+++ src/stores/products.ts
@@ -1,53 +1,12 @@
 import { defineStore } from 'pinia'
 import type { Product } from '@/model/types';
+import productsData from '../data/products.json'
 
 export const useProductsStore = defineStore('products', {
     state: () => ({
         order: 'price' as string,
         categoryId: null as number|null,
-        _products: [
-            {
-                name: 'Silla', 
-                price: 56, id: 1, 
-                image: '/products/silla.webp',
-                categoryId: 1
-            },
-            {
-                name: 'Monitor', 
-                price: 450, 
-                id: 2, 
-                image: '/products/monitor.jpg',
-                categoryId: 1
-            },
-            {
-                name: 'Micrófono', 
-                price: 20, 
-                id: 3, 
-                image: '/products/microfono.jpg',
-                categoryId: 2
-            },
-            {
-                name: 'Parlantes', 
-                price: 56, 
-                id: 4,
-                image: '/products/parlantes.jpg',
-                categoryId: 1
-            },
-            {
-                name: 'Audífonos', 
-                price: 450, 
-                id: 5,
-                image: '/products/headphones.jpg',
-                categoryId: 2
-            },
-            {
-                name: 'Mouse Gamer', 
-                price: 20, 
-                id: 6,
-                image: '/products/mouse.jpg',
-                categoryId: 2
-            }
-        ] as Product[]
+        _products: productsData as Product[]
     }),
     getters: {
         products(state) {
